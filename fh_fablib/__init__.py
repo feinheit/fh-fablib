2ccd67882c6d1eede75ad8a509cccbd5b4675cd8
Matthias Kestenholz <mk@feinheit.ch>
2018-02-20 15:26:12 +0100

    fh-fablib 0.6.3

--- fh_fablib/__init__.py
+++ fh_fablib/__init__.py
@@ -14,7 +14,7 @@ from fabric.contrib.console import confirm
 from fabric.utils import abort, puts
 
 
-VERSION = (0, 6, 2)
+VERSION = (0, 6, 3)
 __version__ = '.'.join(map(str, VERSION))
 
 DEFAULTS = {
