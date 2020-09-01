c50e60ed31b3683d1bd7136a5c86dde444423ea7
Matthias Kestenholz <mk@feinheit.ch>
2020-09-01 11:11:23 +0200

    fh-fablib 1.0.20200901

--- fh_fablib/__init__.py
+++ fh_fablib/__init__.py
@@ -13,7 +13,7 @@ from fabric import Connection, task
 from invoke import Collection  # noqa, re-export
 
 
-__version__ = "1.0.20200827"
+__version__ = "1.0.20200901"
 
 
 # I don't care, in this context.
