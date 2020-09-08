5821cdaa959108d1d7723bf8104d1e40c617ed5e
Matthias Kestenholz <mk@feinheit.ch>
2020-09-08 08:17:46 +0200

    Terminate bumpversion if version does not exist in CHANGELOG.rst

--- bumpversion.sh
+++ bumpversion.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 
-set -ex
+set -e
 
-D=$(date +%Y%m%d)
-sed -i -e "s/__version__ =.*/__version__ = \"1.0.$D\"/" fh_fablib/__init__.py
-git commit -a -m "fh-fablib 1.0.$D"
-git tag -m "fh-fablib 1.0$D" "1.0.$D"
+VERSION="1.0.$(date +%Y%m%d)"
+if ! grep --quiet "$VERSION" CHANGELOG.rst
+then
+    echo "$VERSION not found in CHANGELOG.rst"
+    exit 1
+fi
+
+set -x
+sed -i -e "s/__version__ =.*/__version__ = \"$VERSION\"/" fh_fablib/__init__.py
+git commit -a -m "fh-fablib $VERSION"
+git tag -m "fh-fablib $VERSION" "$VERSION"
